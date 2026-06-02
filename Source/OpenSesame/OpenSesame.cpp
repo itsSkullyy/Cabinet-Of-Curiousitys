4c0b202c7e8d8ebaaa59e9848e77201fdb99e1fa
Skullyy <127060426+itsSkullyy@users.noreply.github.com>
2026-06-02 12:46:17 +1000

    HOLY SHIT I HATE UE5 PROJECTS WHY DID IT BREAK FOR 2 HOURS
    - Had to genuinely rebuild the project in Visual code studio -70GB of free space fuck you UE5

---
+++ Source/OpenSesame/OpenSesame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenSesame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OpenSesame, "OpenSesame" );
